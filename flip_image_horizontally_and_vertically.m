d0e0a2f77246893c19355ddd04c8d5b6946e2cc8
yozaki21jp <114101028+yozaki21jp@users.noreply.github.com>
2023-08-26 14:02:02 -0400

    Update flip_image_horizontally_and_vertically.m
    flip an image horizontally and vertically with MATLAB code

--- flip_image_horizontally_and_vertically.m
+++ flip_image_horizontally_and_vertically.m
@@ -1,5 +1,5 @@
-I = imread("rose1024.tif");
-H=I(:,(end:-1:1),:);
-figure;imshow(H);
-V=I(end:-1:1,:);
-figure;imshow(V);
+I = imread("rose1024.tif");%read an image here.Ex;read an image named "rose1024.tif" and assigned as I
+H=I(:,(end:-1:1),:);%flip the image horizontally
+figure;imshow(H);%show the horizontally fliiped image here
+V=I(end:-1:1,:);%flip the image vertically
+figure;imshow(V);show the vertically fliiped image here
