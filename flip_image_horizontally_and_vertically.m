1b422995a4d7f7d9b2d0b3284a12e369f95deced
yozaki21jp <114101028+yozaki21jp@users.noreply.github.com>
2023-08-26 14:02:23 -0400

    Update flip_image_horizontally_and_vertically.m

--- flip_image_horizontally_and_vertically.m
+++ flip_image_horizontally_and_vertically.m
@@ -2,4 +2,4 @@ I = imread("rose1024.tif");%read an image here.Ex;read an image named "rose1024.
 H=I(:,(end:-1:1),:);%flip the image horizontally
 figure;imshow(H);%show the horizontally fliiped image here
 V=I(end:-1:1,:);%flip the image vertically
-figure;imshow(V);show the vertically fliiped image here
+figure;imshow(V);%show the vertically fliiped image here
